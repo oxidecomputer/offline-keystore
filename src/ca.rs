1e17af09fd3e85a79eaec5fda3b9bcb207744ec3
Philip Tricca <flihp@twobit.org>
2023-03-25 21:23:57 -0700

    Use same default for paths passed to ca initialize and sign.

--- src/ca.rs
+++ src/ca.rs
@@ -163,10 +163,18 @@ pub fn initialize(
     debug!("canonical KeySpec path: {}", key_spec.display());
 
     let paths = if key_spec.is_file() {
-        vec![key_spec]
+        vec![key_spec.clone()]
     } else {
         config::files_with_ext(&key_spec, KEYSPEC_EXT)?
     };
+
+    if paths.is_empty() {
+        return Err(anyhow::anyhow!(
+            "no files with extension \"{}\" found in dir: {}",
+            KEYSPEC_EXT,
+            &key_spec.display()
+        ));
+    }
 
     // start connector
     debug!("starting connector");
